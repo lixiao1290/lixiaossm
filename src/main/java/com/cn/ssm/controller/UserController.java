b4cf29aae96fa0dc62f80f4bbd39e96730d0aa2a
lixiao <lixiao.god@163.com>
2018-05-30 09:58:58 +0800

    shiro

--- src/main/java/com/cn/ssm/controller/UserController.java
+++ src/main/java/com/cn/ssm/controller/UserController.java
@@ -2,12 +2,10 @@ package com.cn.ssm.controller;
 
 import javax.annotation.Resource;
 
-import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import com.cn.ssm.model.User;
 import com.cn.ssm.service.IUserService;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,58 +16,58 @@ import java.util.List;
 @Controller
 @RequestMapping("/user")
 public class UserController {
-	@Resource
-	private IUserService userService;
-	
-//	@RequestMapping("/userInfo")
+    @Resource
+    private IUserService userService;
+
+    //	@RequestMapping("/userInfo")
 //	public String userInfo(HttpServletRequest request,Model model){
 //		int userId = Integer.parseInt(request.getParameter("id"));
 //		User user = this.userService.getUserById(userId);
 //		model.addAttribute("user", user);
 //		return "userInfo";
 //	}
-	@RequestMapping("/userAdd")
-	public String userAdd(@RequestParam int id, Model model){
+    @RequestMapping("/userAdd")
+    public String userAdd(@RequestParam int id, Model model) {
 //		int userId = Integer.parseInt(request.getParameter("id"));
-		User user=new User();
-		int cun = this.userService.addUser(user);
-		model.addAttribute("user", user);
-		return "userInfo";
-	}
+        User user = new User();
+        int cun = this.userService.addUser(user);
+        model.addAttribute("user", user);
+        return "userInfo";
+    }
 
-	@RequestMapping("/userInfo")
-	public String userInfo(@RequestParam int id, Model model) {
+    @RequestMapping("/userInfo")
+    public String userInfo(@RequestParam int id, Model model) {
 //		int userId = Integer.parseInt(request.getParameter("id"));
-		User user = this.userService.getUserById(id);
-		model.addAttribute("user", user);
-		return "userInfo";
-	}
+        User user = this.userService.getUserById(id);
+        model.addAttribute("user", user);
+        return "userInfo";
+    }
 
-	@RequestMapping(path="/userInfoPath/{id}")
-	public String userInfoPath(@PathVariable int id,Model model){
-		User user = this.userService.getUserById(id);
-		model.addAttribute("user",user);
-		return "userInfo";
-	}
+    @RequestMapping(path = "/userInfoPath/{id}")
+    public String userInfoPath(@PathVariable int id, Model model) {
+        User user = this.userService.getUserById(id);
+        model.addAttribute("user", user);
+        return "userInfo";
+    }
 
-	@ResponseBody
-	@RequestMapping(path="/userInfoJson/{id}")
-	public User userInfoJson(@PathVariable int id){
-		User user = this.userService.getUserById(id);
-		return user;
-	}
-	
-	@RequestMapping(path = "/deleteUser/{id}")
-	public String deleteUser(@PathVariable int id) {
-		int count = this.userService.deleteUserById(id);
-		return "userInfo";
-	}
+    @ResponseBody
+    @RequestMapping(path = "/userInfoJson/{id}")
+    public User userInfoJson(@PathVariable int id) {
+        User user = this.userService.getUserById(id);
+        return user;
+    }
 
-	@RequestMapping(path = "/userlist")
-	public String UserList(Model model) {
-		List<User> users=this.userService.getAllUser();
-		model.addAttribute("users",users);
-		model.addAttribute("a","41544");
-		return "userList";
-	}
+    @RequestMapping(path = "/deleteUser/{id}")
+    public String deleteUser(@PathVariable int id) {
+        int count = this.userService.deleteUserById(id);
+        return "userInfo";
+    }
+
+    @RequestMapping(path = "/userlist")
+    public String UserList(Model model) {
+        List<User> users = this.userService.getAllUser();
+        model.addAttribute("users", users);
+        model.addAttribute("a", "41544");
+        return "userList";
+    }
 }
